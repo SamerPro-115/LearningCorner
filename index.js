00d027596e247082386af2fd343e4fe673dd1b27
samer132344 <newsamer123@gmail.com>
2024-01-26 23:36:37 +0300

    Commit

--- index.js
+++ index.js
@@ -1,12 +1,17 @@
 
 
+// const currentPath = window.location.pathname;
+// if(currentPath === "/") {
+
+// }
+
 
 
 
 
 function startCounterWhenVisible() {
   var options = {
-    threshold: 0.5, // Adjust as needed
+    threshold: 0.5, 
   };
 
   var observer = new IntersectionObserver(function (entries, observer) {
@@ -21,7 +26,7 @@ function startCounterWhenVisible() {
               countNum: countTo,
             },
             {
-              duration: 700,
+              duration: 1000,
               easing: 'linear',
               step: function () {
                 $counter.text(Math.floor(this.countNum));
@@ -57,33 +62,7 @@ startCounterWhenVisible();
     
 
 
-    
-    $(document).on("click",".burger-list", function() {
-      const el = `
-      <svg xmlns="http://www.w3.org/2000/svg"  fill="currentColor" class="bi bi-x shadow-x" style="color: #b578bc; display: none; height: 100%;" viewBox="0 0 16 16">
-      <path d="M4.646 4.646a.5.5 0 0 1 .708 0L8 7.293l2.646-2.647a.5.5 0 0 1 .708.708L8.707 8l2.647 2.646a.5.5 0 0 1-.708.708L8 8.707l-2.646 2.647a.5.5 0 0 1-.708-.708L7.293 8 4.646 5.354a.5.5 0 0 1 0-.708"/>
-    </svg>
-      `
-      $(".overlay").fadeIn(400)
-      $(".exit-navbar").fadeIn(400)
-      $(".burger-list").fadeOut(200, function() {
-      $(".burger-list").replaceWith(el);
-       $(".shadow-x").fadeIn(200);
-
-      });
-      $("html").css("overflow", "hidden");
-  });
-  
-  $(".exit-navbar").on("click", function() {
-    $("html").css("overflow", "visible");
-      $(".overlay").fadeOut(400)
-      const el = `<svg xmlns="http://www.w3.org/2000/svg" fill="currentColor" style="display: none;" class="bi bi-list burger-list d-xl-none d-lg-none d-md-none" viewBox="0 0 16 16">
-      <path fill-rule="evenodd" d="M2.5 12a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5m0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5m0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5"/>
-    </svg>`
-
-       $(".container-fluid").find("svg").replaceWith(el)
-       $(".burger-list").fadeIn(400)
-  });
+   
 
 
   function redirectToProductPage(productPageUrl) {
@@ -111,3 +90,34 @@ startCounterWhenVisible();
     $("html").css("overflow", "hidden")
   })
   
+
+   const changingColorElement = document.getElementById('changingColorElement');
+   const changingColorElementMobile = document.getElementById('changingColorElementMobile');
+
+   const colors = ['#7f0d84', '#ffbc04'];
+   
+  
+   let colorIndex = 0;
+ 
+   function isScreenSizeGreaterThan992() {
+    const screenWidth = window.innerWidth || document.documentElement.clientWidth || document.body.clientWidth;
+    
+    return screenWidth > 992;
+  }
+  
+   function updateColor() {
+     if(isScreenSizeGreaterThan992()) {
+      changingColorElement.style.backgroundColor = colors[colorIndex];
+      colorIndex = (colorIndex + 1) % colors.length; // Cycle through colors
+     } else {
+      changingColorElementMobile.style.backgroundColor = colors[colorIndex];
+     colorIndex = (colorIndex + 1) % colors.length; // Cycle through colors
+     }
+   }
+ 
+  
+   setInterval(updateColor, 1000);
+
+
+
+
